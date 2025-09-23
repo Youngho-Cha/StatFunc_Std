8f303865e49d33a1bf80d784c95d3c3115741e04
Youngho-Cha <yh.cha@jnpmedi.com>
2025-09-23 17:23:05 +0900

    Change: {Survival.metrics} create_surv_plot

--- packages/Survival.metrics/R/create_surv_plot.R
+++ packages/Survival.metrics/R/create_surv_plot.R
@@ -6,13 +6,25 @@
 #' @param title The title of the survival curve
 #' @param x The name of the x-axis
 #' @param y The name of the y-axis
+#' @param legend_title The title of the legend
+#' @param legend_labels The names of legend labels
 #'
-#' @importFrom ggplot2 ggplot geom_step geom_ribbon labs theme_minimal ggsave
+#' @importFrom ggplot2 ggplot geom_step geom_ribbon labs theme_minimal ggsave scale_color_discrete scale_fill_discrete
 #' @importFrom broom tidy
+#' @importFrom dplyr bind_rows arrange
 #'
 #' @export
-create_surv_plot=function(km_res,filepath,title,x,y){
+create_surv_plot=function(km_res,filepath,title,x,y,legend_title,legend_labels){
   fit_df=tidy(km_res)
+  start_points=data.frame(
+    time=0,
+    estimate=1,
+    conf.high=1,
+    conf.low=1,
+    strata=unique(fit_df$strata)
+  )
+  fit_df=bind_rows(strata_points,fit_df) %>%
+    arrange(strata,time)
 
   p=ggplot(fit_df,aes(x=time,y=estimate,color=strata))+
     geom_step(linewidth=1.2)+
@@ -20,8 +32,12 @@ create_surv_plot=function(km_res,filepath,title,x,y){
     labs(
       title=title,
       x=x,
-      y=y
+      y=y,
+      color=legend_title,
+      fill=legend_title
     )+
+    scale_color_discrete(labels=legend_labels)+
+    scale_fill_discrete(labels=legend_labels)+
     theme_minimal()
 
   ggsave(
